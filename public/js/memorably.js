b265ece4ad94cb43c7196ee0337a3334bb08b400
Amarnath Krishnan <Amarnath.Krishnan@in.tesco.com>
2017-08-02 21:48:08 +0530

    auto close the menu on click

--- public/js/memorably.js
+++ public/js/memorably.js
@@ -5,7 +5,12 @@ $(document).ready(function () {
     var position = ($('.'+section).offset().top - 56);
     var parent = $(this).parent();
     parent.addClass('active');
+    // stop url navigation
     e.preventDefault();
+    // to auto close menu
+    $('.navbar-header button').trigger('click');
+    // to scroll to that position
     $("html, body").animate({ scrollTop: position });
+
   });
 });
